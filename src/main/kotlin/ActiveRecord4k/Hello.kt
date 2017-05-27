d9d3afd19c87c53ec5a0964b880bf7c13b51829a
icepoint0 <351711778@qq.com>
2017-05-27 10:12:52 +0800

    first commit

---
+++ src/main/kotlin/ActiveRecord4k/Hello.kt
@@ -0,0 +1,7 @@
+package ActiveRecord4k
+
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/ActiveRecord4k/HelloTest.kt
@@ -0,0 +1,8 @@
+package ActiveRecord4k
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
